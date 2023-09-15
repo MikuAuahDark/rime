cd81593c7ec0c5dedf4ffee245220250025e5d51
Miku AuahDark <lumi.hotaru@gmail.com>
2023-09-15 13:05:03 +0800

    Introduce ESLint.
    Note that you must install them yourself if you want to use it.

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab",
+			{"SwitchCase": 1}
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"no-unused-vars": 1,
+	}
+}
